293252b689dec7545f7dcd30c72e01ca25609b2d
Jakub Michalica <jamesak252@gmail.com>
2024-09-21 11:33:52 +0200

    Finished gamspy quick start example

--- gamspy-learning/qs_transportation_problem.py
+++ gamspy-learning/qs_transportation_problem.py
@@ -1,5 +1,6 @@
 from gamspy import Container, Set, Parameter, Variable, Equation, Model, Sum, Sense, Options
-
+import pandas as pd
+import sys
 
 if __name__ == "__main__":
     
@@ -83,4 +84,76 @@ if __name__ == "__main__":
         sense=Sense.MIN, # Minimise
         objective=obj
     )
-    pass
+    
+    # Data
+    i.setRecords(["seattle", "san-diego"])
+    j.setRecords(["new-york", "chicago", "topeka"])
+    a.setRecords([("seattle", 350), ("san-diego", 600)])
+    b.setRecords([("new-york", 325), ("chicago", 300), ("topeka", 275)])
+    # print(i.records)
+    # print(b.records)
+    
+    ## Distances
+    distances = pd.DataFrame(
+        [
+            ["seattle", "new-york", 2.5],
+            ["seattle", "chicago", 1.7],
+            ["seattle", "topeka", 1.8],
+            ["san-diego", "new-york", 2.5],
+            ["san-diego", "chicago", 1.8],
+            ["san-diego", "topeka", 1.4],
+        ],
+        columns=["from", "to", "distance"]
+    ).set_index(["from", "to"])
+    
+    d = Parameter(
+        container=m,
+        name="d",
+        domain=[i, j],
+        description="distance between plant i and market j",
+        records=distances.reset_index(),
+    )
+    print(d.records)
+    
+    ## Calculating cost per unit of shipment between plant i and market j
+    ### 1. Python assignment
+    freight_cost = 90
+    # cost = freight_cost * distances / 1000
+    # c.setRecords(cost.reset_index())
+    # print(c.records)
+    
+    ### 2. GAMSpy assignment
+    c[i, j] = freight_cost * d[i, j] / 1000
+    # print(c.records)
+    
+    # Solve
+    ## To view the output of the solver use sys library
+    
+    # Verifying generated model
+    ## To view generated equations and variables use
+    ### getEquationListing and getVariableListing
+    ### but we first need to solve the model with column_listing_limit and variable_listing_limit
+    
+    transport.solve(
+        output=sys.stdout,
+        options=Options(
+            equation_listing_limit=10,
+            variable_listing_limit=10,
+        )
+    )
+    
+    # Inspect generated equations and variables
+    print(transport.getEquationListing())
+    print(transport.getVariableListing())
+    
+    # Same can be called on the individual equation and variable symbols
+    print(supply.getEquationListing())
+    print(x.getVariableListing())
+    
+    
+    # Retrieving results
+    ## Variable results
+    print(x.records)
+    ## Objective values
+    print(transport.objective_value)
+    
